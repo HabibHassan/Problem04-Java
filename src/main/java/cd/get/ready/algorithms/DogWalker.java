93b51d1cab8982f1059ff5d94dd301e13489d6f8
Habib's World <62362787+HabibHassan@users.noreply.github.com>
2020-10-06 01:38:30 -0400

    Add files via upload
    Problem 4 My Solution

--- src/main/java/cd/get/ready/algorithms/DogWalker.java
+++ src/main/java/cd/get/ready/algorithms/DogWalker.java
@@ -1,8 +1,18 @@
 package cd.get.ready.algorithms;
+import java.util.*;
 
 public class DogWalker {
     public int numberOfWalksCalculator(int[] dogSizes){
-        int numberOfWalks = -1;
+        
+        int numberOfWalks = 0;
+        Hashset<Integer> num = new Hashset<Integer>();
+        
+        for(int i = 0; i < dogSizes.length; i++){
+        num.add(dogSizes[i]);
+        numberOfWalks = num.size();
+        
+        
+    }
         return numberOfWalks;
     }
 }
